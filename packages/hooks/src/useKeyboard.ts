ce669875fdd4590e90ecf207d8d6af729ea53ca6
malkiAbdoo <abdo.malkiep@gmail.com>
2023-07-17 22:12:29 +0100

    chore(hooks): remove element target in useKeyboard hook

--- packages/hooks/src/useKeyboard.ts
+++ packages/hooks/src/useKeyboard.ts
@@ -1,18 +1,13 @@
-import { KeyboardEventHandler, useCallback, useEffect, useRef } from 'react';
+import { KeyboardEventHandler, useCallback, useEffect } from 'react';
 import hotkeys from 'hotkeys-js';
 
 export const useKeyboard = (keysRecord: Record<string, KeyboardEventHandler>) => {
-  const targetRef = useRef<HTMLElement>();
-
   const keys = Object.keys(keysRecord);
-  const options: any = targetRef.current ? { element: targetRef } : {};
 
   const bindAllKeys = useCallback(() => {
-    keys.forEach(key => hotkeys(key, options, keysRecord[key] as any));
+    keys.forEach(key => hotkeys(key, keysRecord[key] as any));
     return () => keys.forEach(key => hotkeys.unbind(key));
   }, [keysRecord]);
 
   useEffect(bindAllKeys, [keysRecord]);
-
-  return targetRef;
 };
