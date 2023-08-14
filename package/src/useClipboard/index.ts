35d195a331d7345f08cde2f4a6c6975727c40048
malkiAbdoo <abdo.malkiep@gmail.com>
2023-08-14 22:02:45 +0100

    feat(hook): create `useClipboard` hook

---
+++ package/src/useClipboard/index.ts
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { useTimeout } from '@/src';
+
+export const useClipboard = ({ timeout = 3000 } = {}) => {
+  const [copied, setCopied] = useState<boolean>(false);
+  const [error, setError] = useState<Error>();
+
+  const statusTimer = useTimeout(() => setCopied(false), timeout);
+
+  return {
+    copy(text?: string) {
+      navigator.clipboard
+        .writeText(text || '')
+        .then(() => {
+          statusTimer.start();
+          setCopied(true);
+        })
+        .catch(error => setError(error));
+    },
+    reset() {
+      statusTimer.stop();
+      setCopied(false);
+      setError(undefined);
+    },
+    copied,
+    error
+  } as const;
+};
