d3999c3e0bd1bb83632744be77aa3d870ec7c10b
malkiAbdoo <abdo.malkiep@gmail.com>
2023-08-14 18:54:58 +0100

    refactor(script): update the export script

--- package/export-hooks.sh
+++ package/export-hooks.sh
@@ -2,14 +2,14 @@
 
 src_directory="$(dirname "$0")/src"
 index_file="$src_directory/index.ts"
-hooks_files=$(find $src_directory -maxdepth 1 -type d -name 'use*')
+hooks_folders=$(find $src_directory -maxdepth 1 -type d -name 'use*')
 
 # Create or overwrite the index.ts file
 echo "/** @run \"pnpm prebuild\" to modify this file */" > $index_file
 
-for file in ${hooks_files[@]}; do
-  filename=$(basename $file)
-  exported_hooks=$(grep -oP "(?<=export const )use\w+" "$file/index.ts" | sed ':a;N;$!ba;s/\n/, /g')
+for folder in ${hooks_folders[@]}; do
+  folder_name=$(basename $folder)
+  exported_hooks=$(grep -oP "(?<=export const )use\w+" "$folder/index.ts" | sed ':a;N;$!ba;s/\n/, /g')
 
-  echo "export { $exported_hooks } from './$filename';" >> $index_file
+  echo "export { $exported_hooks } from './$folder_name';" >> $index_file
 done
