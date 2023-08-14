c83289797e13d6a206018bbf5291d8f17dfee2dd
malkiAbdoo <abdo.malkiep@gmail.com>
2023-08-14 18:55:59 +0100

    build: add `tsconfigPaths` plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import banner from 'vite-plugin-banner';
 import dts from 'vite-plugin-dts';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 import { author, name, version } from './package.json';
 
@@ -9,7 +10,7 @@ const bannerText = `${name} v${version} Copyright 2023 ${author.name}`;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [banner(bannerText), react(), dts({ insertTypesEntry: true })],
+  plugins: [tsconfigPaths(), dts({ insertTypesEntry: true }), react(), banner(bannerText)],
   build: {
     lib: {
       name,
