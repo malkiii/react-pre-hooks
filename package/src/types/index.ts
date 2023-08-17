c0adb61a0899437ea86761527e879b74b1397199
malkiAbdoo <abdo.malkiep@gmail.com>
2023-08-17 11:49:36 +0100

    fix(types): remove unused types

--- package/src/types/index.ts
+++ package/src/types/index.ts
@@ -1,8 +1,6 @@
-import { Dispatch, MutableRefObject, RefObject, SetStateAction } from 'react';
+import { RefObject } from 'react';
 
 export type NonEmptyArray<T> = [T, ...T[]];
-
-export type StateSetter<T = any> = Dispatch<SetStateAction<T>>;
 export type WithRef<P = {}, T = HTMLElement> = P & {
-  ref?: RefObject<T> | MutableRefObject<T>;
+  ref?: RefObject<T>;
 };
