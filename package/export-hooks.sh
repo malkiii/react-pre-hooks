11bc9c8fcac05e9aedea166e78f375a364db2364
malkiAbdoo <abdo.malkiep@gmail.com>
2023-08-25 22:58:46 +0100

    fix(script): export hooks types

--- package/export-hooks.sh
+++ package/export-hooks.sh
@@ -7,9 +7,21 @@ hooks_folders=$(find $src_directory -maxdepth 1 -type d -name 'use*')
 # Create or overwrite the index.ts file
 echo "/** @run \"pnpm prebuild\" to modify this file */" > $index_file
 
-for folder in ${hooks_folders[@]}; do
-  folder_name=$(basename $folder)
-  exported_hooks=$(grep -oP "(?<=export const )use\w+" "$folder/index.ts" | sed ':a;N;$!ba;s/\n/, /g')
+export_with() {
+  for folder in ${hooks_folders[@]}; do
+    folder_name=$(basename $folder)
+    exports=$(grep -oP "$2" "$folder/index.ts" | sed ':a;N;$!ba;s/\n/, /g')
 
-  echo "export { $exported_hooks } from './$folder_name';" >> $index_file
-done
+    if [ -n "$exports" ]; then
+      echo "export $1{ $exports } from './$folder_name';" >> $index_file
+    fi
+  done
+}
+
+# export hooks
+export_with "" "(?<=export const )use\w+"
+
+echo "" >> $index_file
+
+# export types
+export_with "type " "(?<=export type )\w+"
