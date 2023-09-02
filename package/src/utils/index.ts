3fad21f84a43875750168b6965a28c6393aa4c1b
malkiAbdoo <abdo.malkiep@gmail.com>
2023-09-02 16:22:11 +0100

    feat: create download function

--- package/src/utils/index.ts
+++ package/src/utils/index.ts
@@ -8,3 +8,14 @@ export function getCurrentMousePosition(event: MouseEvent | TouchEvent) {
     ? { x: event.offsetX, y: event.offsetY }
     : { x: event.changedTouches[0].clientX, y: event.changedTouches[0].clientY };
 }
+
+export function download(url: string, name = '') {
+  const a = document.createElement('a');
+  a.style.display = 'none';
+  a.href = url;
+  a.download = name;
+
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+}
