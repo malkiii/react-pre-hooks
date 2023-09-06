3483dafaf6b01b1c3e0bfff3a759b1b44745d3dd
malkiAbdoo <abdo.malkiep@gmail.com>
2023-09-06 23:43:04 +0100

    feat(hook): add useSpeech hook

---
+++ package/src/useSpeech/index.ts
@@ -0,0 +1,116 @@
+import { SetStateAction, useCallback, useLayoutEffect, useMemo, useRef, useState } from 'react';
+
+export type SpeechOptions = Partial<
+  Pick<SpeechSynthesisUtterance, 'lang' | 'voice' | 'rate' | 'pitch' | 'volume'>
+>;
+
+export const useSpeech = (options: SpeechOptions = {}) => {
+  const { lang, voice = null, rate = 1, pitch = 1, volume = 1 } = options;
+
+  const charIndex = useRef<number>(0);
+  const speechRef = useRef(new SpeechSynthesisUtterance());
+  const [voices, setVoices] = useState<SpeechSynthesisVoice[]>([]);
+  const [state, setState] = useState({
+    lang,
+    voice,
+    rate,
+    pitch,
+    volume,
+    text: '',
+    isSpeeking: false,
+    isPaused: false,
+    isEnded: false
+  });
+
+  const controls = useMemo(
+    () => ({
+      speek(text: string) {
+        startSpeech(text);
+        setState(s => ({ ...s, text: '' }));
+      },
+      pause() {
+        if (speechSynthesis.speaking) speechSynthesis.pause();
+      },
+      resume() {
+        if (speechSynthesis.paused) speechSynthesis.resume();
+      },
+      cancel() {
+        this.resume();
+        speechSynthesis.cancel();
+      },
+      setLang(lang: SetStateAction<string | undefined>) {
+        setResolvedValue('lang', lang);
+      },
+      setVoice(voice: SetStateAction<SpeechSynthesisVoice | null>) {
+        setResolvedValue('voice', voice);
+      },
+      setRate(rate: SetStateAction<number>) {
+        setResolvedValue('rate', rate);
+      },
+      setPitch(pitch: SetStateAction<number>) {
+        setResolvedValue('pitch', pitch);
+      },
+      setVolume(volume: SetStateAction<number>) {
+        setResolvedValue('volume', volume);
+      },
+      setCharIndex(index: number) {
+        if (!speechRef.current || !speechSynthesis.speaking) return;
+        startSpeech(speechRef.current.text.substring(index));
+      }
+    }),
+    [speechRef]
+  );
+
+  const setResolvedValue = useCallback(
+    <K extends keyof SpeechOptions>(prop: K, value: SetStateAction<(typeof state)[K]>) => {
+      if (!speechRef.current) return;
+      setState(state => {
+        speechRef.current![prop] = value instanceof Function ? value(state[prop]) : (value as any);
+        controls.setCharIndex(charIndex.current);
+        return { ...state, [prop]: speechRef.current![prop] };
+      });
+    },
+    [controls]
+  );
+
+  const startSpeech = useCallback(
+    (text: string) => {
+      if (!speechRef.current.voice) return;
+      const isPaused = speechSynthesis.paused;
+      controls.cancel();
+
+      speechRef.current.text = text;
+      speechSynthesis.speak(speechRef.current);
+      if (isPaused) speechSynthesis.pause();
+    },
+    [controls]
+  );
+
+  useLayoutEffect(() => {
+    const speech = speechRef.current;
+    if (lang) speech.lang = lang;
+    speech.rate = rate;
+    speech.pitch = pitch;
+    speech.volume = volume;
+
+    speech.onstart = () => setState(s => ({ ...s, isSpeeking: true, isEnded: false }));
+    speech.onend = () => setState(s => ({ ...s, isSpeeking: false, isEnded: true }));
+    speech.onpause = () => setState(s => ({ ...s, isSpeeking: false, isPaused: true }));
+    speech.onresume = () => setState(s => ({ ...s, isSpeeking: true, isPaused: false }));
+    speech.onboundary = e => {
+      charIndex.current = e.charIndex;
+      const nextWord = speechRef.current.text.substring(e.charIndex).split(/\s+/g).shift() ?? '';
+      setState(s => ({ ...s, text: s.text + (s.text.endsWith(nextWord) ? '' : ' ' + nextWord) }));
+    };
+
+    speechSynthesis.onvoiceschanged = () => {
+      const voices = speechSynthesis.getVoices().filter(v => v.localService);
+      speechRef.current.voice = voice ?? voices.find(v => v.default)!;
+      setVoices(voices);
+    };
+
+    return () => controls.cancel();
+  }, []);
+
+  return { ...controls, ...state, voices };
+};
