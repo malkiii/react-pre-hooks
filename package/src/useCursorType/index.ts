05c2d6b914f78f3bd3a18d71b07376ceba6eb775
malkiAbdoo <abdo.malkiep@gmail.com>
2023-09-09 22:56:47 +0100

    feat(hook): add useCursorType hook

---
+++ package/src/useCursorType/index.ts
@@ -0,0 +1,20 @@
+import { CSSProperties, RefObject, useCallback, useRef, useState } from 'react';
+import { useEventListener } from '@/src';
+
+export type CursorType = CSSProperties['cursor'];
+
+export const useCursorType = <T extends HTMLElement = HTMLDivElement>(ref?: RefObject<T>) => {
+  const targetRef = ref || useRef<T>(null);
+  const [cursor, setCursor] = useState<CursorType>('auto');
+
+  const handleMouseMove = useCallback((event: MouseEvent) => {
+    if (!event.target) return;
+    setCursor(window.getComputedStyle(event.target as any).cursor);
+  }, []);
+
+  useEventListener(['mousemove', 'mousedown', 'mouseup'], handleMouseMove, {
+    target: targetRef.current || window
+  });
+
+  return { targetRef, cursor };
+};
