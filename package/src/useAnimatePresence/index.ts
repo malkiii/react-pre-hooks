6f3a428fec4115fb839668e4731bd68f75788a84
malkiAbdoo <abdo.malkiep@gmail.com>
2023-09-15 19:01:06 +0100

    perf(useAnimatePresense): rewrite the hook using `animate` API

--- package/src/useAnimatePresence/index.ts
+++ package/src/useAnimatePresence/index.ts
@@ -1,122 +1,83 @@
 import { CSSProperties, RefObject, SetStateAction, useCallback, useRef, useState } from 'react';
-import { getStateActionValue } from '@/src/utils';
 
-export type TransitionProps = Partial<{
-  ease: CSSProperties['transitionTimingFunction'];
-  duration: number;
-  delay: number;
-}>;
-
-export type TransformProps = Partial<{
-  opacity: string;
-  translate: string;
-  scale: string;
-  rotate: string;
-}>;
-
-export type AnimationProps = TransformProps & {
-  transition?: TransitionProps;
+export type TransitionProps = Omit<KeyframeAnimationOptions, 'easing'> & {
+  easing: CSSProperties['transitionTimingFunction'];
 };
 
 export type AnimatePresenceOptions<T extends HTMLElement = HTMLElement> = Partial<{
   ref: RefObject<T>;
   initialMount: boolean;
-  animate: AnimationProps;
-  exit: AnimationProps;
+  keyframes: PropertyIndexedKeyframes;
+  transition: TransitionProps;
   onEnter: (element: T) => any;
   onExit: (element: T) => any;
-  transition: TransitionProps;
 }>;
 
 export const useAnimatePresence = <T extends HTMLElement = HTMLDivElement>(
   options: AnimatePresenceOptions<T> = {}
 ) => {
-  const { initialMount = false, animate, exit, transition, onEnter, onExit } = options;
+  const { initialMount = false, keyframes = {}, onEnter, onExit } = options;
 
   const ref = options.ref || useRef<T>(null);
+  const animationRef = useRef<Animation>();
+  const isAnimating = useRef<boolean>(false);
   const isToggled = useRef<boolean>(initialMount);
-  const durationTimeout = useRef<ReturnType<typeof setTimeout>>();
   const [isMounted, setIsMounted] = useState<boolean>(initialMount);
 
-  const getEase = (props?: TransitionProps) => props?.ease ?? transition?.ease ?? 'ease';
-  const getDuration = (props?: TransitionProps) => props?.duration ?? transition?.duration ?? 300;
-  const getDelay = (props?: TransitionProps) => props?.delay ?? transition?.delay ?? 0;
+  const startAnimation = useCallback(
+    (reverse: boolean = false) => {
+      const transition = options.transition ?? ({} as TransitionProps);
+      if (!transition.easing) transition.easing = 'ease';
+      if (!transition.duration) transition.duration = 300;
+      transition.direction = reverse ? 'reverse' : 'normal';
+      transition.fill = 'both';
 
-  const setProperties = useCallback((animation: AnimationProps = {}) => {
-    const { transition: _, ...props } = animation;
-    Object.entries(props).forEach(pv => ref.current!.style.setProperty(...pv));
-  }, []);
+      if (isAnimating.current && animationRef.current) animationRef.current.reverse();
+      else animationRef.current = ref.current?.animate(keyframes, transition);
 
-  const removeProperties = useCallback((animation: AnimationProps = {}) => {
-    const { transition: _, ...props } = animation;
-    Object.entries(props).forEach(([p, _]) => ref.current!.style.removeProperty(p));
-  }, []);
-
-  const setTransition = useCallback((animation: AnimationProps = {}) => {
-    const { transition: _, ...props } = animation;
-
-    const transitionProperty = Object.keys(props)
-      .filter(prop => !!props[prop as keyof typeof props])
-      .join(',');
-
-    ref.current!.style.transitionProperty = transitionProperty;
-    ref.current!.style.transitionDuration = getDuration(animation?.transition) + 'ms';
-    ref.current!.style.transitionTimingFunction = getEase(animation?.transition);
-  }, []);
-
-  const renderAnimationBetween = useCallback(
-    (initial?: AnimationProps, animation?: AnimationProps) => {
-      setTimeout(() => {
-        setTransition(animation ?? initial);
-        animation ? setProperties(animation) : removeProperties(initial);
-      }, 0);
+      isAnimating.current = true;
     },
-    [setTransition, setProperties, removeProperties]
+    [options]
   );
 
   // mounting animation
   const mount = useCallback(() => {
     isToggled.current = true;
-    if (durationTimeout.current) clearTimeout(durationTimeout.current);
-    else if (isMounted) return;
+    if (!isAnimating.current && isMounted) return;
 
-    const resolvedTransition = animate?.transition ?? exit?.transition;
-    durationTimeout.current = setTimeout(() => {
+    setTimeout(() => {
       setIsMounted(true);
-
       setTimeout(() => {
         if (!ref.current) return setIsMounted(false);
-        setProperties(exit);
-        renderAnimationBetween(exit, animate);
 
-        durationTimeout.current = setTimeout(() => {
-          if (onEnter) onEnter(ref.current!);
-        }, getDuration(resolvedTransition));
+        startAnimation();
+
+        animationRef.current!.onfinish = () => {
+          isAnimating.current = false;
+          onEnter && onEnter(ref.current!);
+        };
       }, 0);
-    }, getDelay(resolvedTransition));
-  }, [renderAnimationBetween]);
+    }, 0);
+  }, [isMounted]);
 
   // unmounting animation
   const unmount = useCallback(() => {
     isToggled.current = false;
-    if (durationTimeout.current) clearTimeout(durationTimeout.current);
-    else if (!isMounted) return;
-    if (!ref.current) return;
+    if (!ref.current || (!isAnimating.current && !isMounted)) return;
 
-    const resolvedTransition = exit?.transition ?? animate?.transition;
-    durationTimeout.current = setTimeout(() => {
-      renderAnimationBetween(animate, exit);
+    startAnimation(true);
 
-      durationTimeout.current = setTimeout(() => {
-        setIsMounted(false);
-        if (onExit) onExit(ref.current!);
-      }, getDuration(resolvedTransition));
-    }, getDelay(resolvedTransition));
-  }, [renderAnimationBetween]);
+    animationRef.current!.onfinish = () => {
+      isAnimating.current = false;
+      setIsMounted(false);
+      if (onExit) onExit(ref.current!);
+    };
+  }, [isMounted]);
 
   const toggle = useCallback(
     (value?: SetStateAction<boolean>) => {
-      const shouldToggle = getStateActionValue(value ?? !isToggled.current, isToggled.current);
+      const resolvedValue = value instanceof Function ? value(isToggled.current) : value;
+      const shouldToggle = resolvedValue ?? !isToggled.current;
       shouldToggle ? mount() : unmount();
     },
     [mount, unmount]
