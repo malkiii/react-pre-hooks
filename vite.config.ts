6dfa927531b08208f8b3f3099b2f816f96715208
malkiAbdoo <abdo.malkiep@gmail.com>
2023-09-17 16:05:43 +0100

    test: remove html coverage

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
     environment: 'jsdom',
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'html']
+      reporter: ['text']
     },
     globals: true
   }
