3564ab1a690079ddac2c6556cf6539d41a8cf009
malkiAbdoo <abdo.malkiep@gmail.com>
2023-09-30 12:17:51 +0100

    refactor(useToggle): rename options to values

--- package/src/useToggle.ts
+++ package/src/useToggle.ts
@@ -1,21 +1,21 @@
 import { SetStateAction, useCallback, useState } from 'react';
 
-export const useToggle = <T extends any = boolean>(options: T[] = [false, true] as any) => {
+export const useToggle = <T extends any = boolean>(values: T[] = [false, true] as any) => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
 
   const toggle = useCallback(
     (value?: SetStateAction<T>) => {
-      if (!options.length) return;
+      if (!values.length) return;
       setCurrentIndex(index => {
-        const currentValue = options[index];
+        const currentValue = values[index];
         const newValue = value instanceof Function ? value(currentValue) : value;
-        if (newValue) return options.includes(newValue) ? options.indexOf(newValue) : index;
+        if (newValue) return values.includes(newValue) ? values.indexOf(newValue) : index;
 
-        return index === options.length - 1 ? 0 : index + 1;
+        return index === values.length - 1 ? 0 : index + 1;
       });
     },
-    [options, currentIndex]
+    [values, currentIndex]
   );
 
-  return [options[currentIndex], toggle] as const;
+  return [values[currentIndex], toggle] as const;
 };
