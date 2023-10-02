c2993ca58fdbdb339fce9a05d6f93817318e0ed5
malkiAbdoo <abdo.malkiep@gmail.com>
2023-10-02 11:11:08 +0100

    fix(useStateStatus): update the types

--- package/src/useStateStatus.ts
+++ package/src/useStateStatus.ts
@@ -1,13 +1,13 @@
 import { SetStateAction, useCallback, useState } from 'react';
 
-export type StatusHandler<T extends any, S = string> = (value: T) => S | null;
+export type StatusHandler<T extends any, S = string> = (value: T) => S | undefined;
 
-export const useStateStatus = <T extends any, S extends string = string>(
+export const useStateStatus = <T extends any, S = string>(
   initialValue: T,
   statusHandler: StatusHandler<T, S>
 ) => {
   const [value, setValue] = useState(initialValue);
-  const [status, setStatus] = useState<S | null>(statusHandler(initialValue));
+  const [status, setStatus] = useState<S | undefined>(statusHandler(initialValue));
 
   const setState = useCallback(
     (state: SetStateAction<T>) => {
