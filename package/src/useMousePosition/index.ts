7990945505604d8a2573393f64f1bf4b51998224
malkiAbdoo <abdo.malkiep@gmail.com>
2023-10-30 21:19:11 +0100

    feat(hooks): add useMousePosition hook

---
+++ package/src/useMousePosition/index.ts
@@ -0,0 +1,18 @@
+import { RefObject, useEffect, useRef, useState } from 'react';
+import { addEvents, getCurrentMousePosition } from '../utils';
+import { useNewRef } from '../utils/useNewRef';
+
+export const useMousePosition = <T extends HTMLElement = HTMLDivElement>(ref?: RefObject<T>) => {
+  const targetRef = useNewRef<T>(ref);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      setPosition(getCurrentMousePosition(event));
+    };
+
+    return addEvents('mousemove', handleMouseMove, { ref: targetRef.current ?? window });
+  }, []);
+
+  return { ref: targetRef, ...position };
+};
