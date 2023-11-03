6ed974b0e44bba5771f0e3c49508d8f63e741ca8
malkiAbdoo <abdo.malkiep@gmail.com>
2023-11-03 14:37:51 +0100

    fix(useSelection): replace isSelecting with isCollapsed state

--- package/src/useSelection/index.ts
+++ package/src/useSelection/index.ts
@@ -7,25 +7,22 @@ export const useSelection = <T extends HTMLElement = HTMLDivElement>(ref?: RefOb
 
   const [text, setText] = useState<string>('');
   const [rect, setRect] = useState<DOMRect>();
-  const [isSelecting, setIsSelecting] = useState<boolean>(false);
+  const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
 
   const handleSelectionChange = useCallback(() => {
     const currentSelection = document.getSelection();
     const target = currentSelection?.focusNode?.parentElement;
-    const isSelecting = !currentSelection?.isCollapsed;
+    const isCollapsed = !!currentSelection?.isCollapsed;
 
     if (targetRef.current && !targetRef.current.contains(target!)) return;
 
-    setIsSelecting(isSelecting);
+    setIsCollapsed(isCollapsed);
     setText(currentSelection?.toString() ?? '');
-    if (isSelecting) setRect(currentSelection?.getRangeAt(0).getBoundingClientRect());
+    if (isCollapsed) setRect(currentSelection?.getRangeAt(0).getBoundingClientRect());
   }, [targetRef]);
 
-  const eventOptions = { target: document, passive: true };
-
   useEffect(handleSelectionChange, []);
-  useEventListener('selectionchange', handleSelectionChange, eventOptions);
-  useEventListener(['mouseup', 'touchend'], () => setIsSelecting(false), eventOptions);
+  useEventListener('selectionchange', handleSelectionChange, { target: document, passive: true });
 
-  return { ref: targetRef, text, rect, isSelecting };
+  return { ref: targetRef, text, rect, isCollapsed };
 };
