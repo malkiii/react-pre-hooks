afa4d198a0e5bd2fb3fe2b7eefcf66fb579f8352
malkiAbdoo <abdo.malkiep@gmail.com>
2023-11-17 10:41:08 +0100

    fix(useIntersectionObserver): replace `root` property with `container` element function property

--- package/src/useIntersectionObserver/index.ts
+++ package/src/useIntersectionObserver/index.ts
@@ -1,7 +1,11 @@
 import { RefObject, useEffect, useRef } from 'react';
 import { useNewRef } from '../utils/useNewRef';
 
-export type IntersectionObserverOptions<T extends HTMLElement> = IntersectionObserverInit & {
+export type IntersectionObserverOptions<T extends HTMLElement> = Omit<
+  IntersectionObserverInit,
+  'root'
+> & {
+  container?: () => Element | Document | null;
   ref?: RefObject<T> | null;
 };
 
@@ -9,13 +13,16 @@ export const useIntersectionObserver = <T extends HTMLElement = HTMLDivElement>(
   handler: IntersectionObserverCallback,
   options: IntersectionObserverOptions<T> = {}
 ) => {
+  const { ref, container, ...observerInit } = options;
+
   const targetRef = useNewRef<T>(options.ref);
   const observer = useRef<IntersectionObserver>();
 
   useEffect(() => {
     if (!targetRef.current) return;
 
-    observer.current = new IntersectionObserver(handler, options);
+    const root = container ? container() : undefined;
+    observer.current = new IntersectionObserver(handler, { ...observerInit, root });
     observer.current.observe(targetRef.current);
 
     return () => observer.current?.disconnect();
