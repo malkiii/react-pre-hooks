eb2852e946f37e857f7938b28bd78d98b40d77e8
malkiAbdoo <abdo.malkiep@gmail.com>
2023-11-17 11:35:08 +0100

    fix(useNetworkState): update useEventListener options

--- package/src/useNetworkState/index.ts
+++ package/src/useNetworkState/index.ts
@@ -45,8 +45,10 @@ export const useNetworkState = () => {
     updateConnection();
   }, []);
 
-  useEventListener(['change', 'typechange' as any], updateConnection, { target: connection });
-  useEventListener(['online', 'offline'], () => setIsOnline(navigator.onLine), { target: window });
+  useEventListener(['change', 'typechange' as any], updateConnection, { target: () => connection });
+  useEventListener(['online', 'offline'], () => setIsOnline(navigator.onLine), {
+    target: () => window
+  });
 
   return { ...networkInfo, isOnline };
 };
