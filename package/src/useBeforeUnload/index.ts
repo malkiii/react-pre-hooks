ea13033e02bd8f6f667fa1a3631abe40fd9cb541
malkiAbdoo <abdo.malkiep@gmail.com>
2024-03-02 18:04:22 +0100

    feat(hooks): add useBeforeUnload and useBoolean hooks

---
+++ package/src/useBeforeUnload/index.ts
@@ -0,0 +1,20 @@
+import { useCallback } from 'react';
+import { useEventListener } from '..';
+
+export const useBeforeUnload = (returnValue: unknown) => {
+  const handleBeforeUnload = useCallback(
+    async (event: BeforeUnloadEvent) => {
+      const value = returnValue instanceof Function ? await returnValue() : returnValue;
+      if (!value) return;
+
+      // For IE6-8 and Firefox prior to version 4
+      event.preventDefault();
+      event.returnValue = value;
+
+      return value; // For Chrome, Safari, IE8+ and Opera 12+
+    },
+    [returnValue]
+  );
+
+  useEventListener('beforeunload', handleBeforeUnload, { target: () => window });
+};
