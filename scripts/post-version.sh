40888d676d2f1b57d7d0d55669aaa8283571f490
malkiAbdoo <abdo.malkiep@gmail.com>
2024-04-11 20:06:05 +0100

    fix: update the version script

--- scripts/post-version.sh
+++ scripts/post-version.sh
@@ -3,5 +3,4 @@
 ROOT_DIR="$(dirname $(dirname "$0"))"
 PACKAGE_VERSION="v$(node -pe "require('$ROOT_DIR/packages/hooks/package.json').version")"
 
-# git tag $PACKAGE_VERSION && git push --tags
-echo $PACKAGE_VERSION;
+git tag $PACKAGE_VERSION && git push --tags
