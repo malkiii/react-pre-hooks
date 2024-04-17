d5ace128c4bdabba662a362951cc4364796bb5e4
malkiAbdoo <abdo.malkiep@gmail.com>
2024-04-17 10:41:07 +0100

    docs: initialize nextra

--- docs/.vitepress/theme/index.js
+++
@@ -1,4 +0,0 @@
-import DefaultTheme from 'vitepress/theme';
-import './globals.css';
-
-export default DefaultTheme;
---
+++ docs/constants/index.ts
@@ -0,0 +1,11 @@
+import pkg from '../../packages/hooks/package.json';
+
+export const site = {
+  title: pkg.name,
+  description: pkg.description,
+  github: pkg.repository.url,
+  author: 'Malki Abderrahmane',
+  url: pkg.homepage,
+  icon: '/logo.svg',
+  og: '/og.png'
+} as const;
