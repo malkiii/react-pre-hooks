7416a61925b84c9d1fdec25cb646ad7f25d6af48
malkiAbdoo <abdo.malkiep@gmail.com>
2024-04-23 15:36:58 +0100

    build: update the script utils

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,7 +1,8 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-export const rootDir = path.join(import.meta.dirname, '../');
+export const rootDir = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../');
 export const hooksFolders = fs
   .readdirSync(path.join(rootDir, 'packages/hooks/src'), { withFileTypes: true })
   .filter(file => file.isDirectory() && file.name.startsWith('use'));
