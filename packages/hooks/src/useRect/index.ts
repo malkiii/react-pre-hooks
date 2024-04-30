3b4163e2e5f6d47eb8ceb870eb758881f9060209
malkiAbdoo <abdo.malkiep@gmail.com>
2024-04-30 22:04:49 +0100

    chore: rename useClientRect to useRect

--- packages/hooks/src/useRect/index.ts
+++ packages/hooks/src/useRect/index.ts
@@ -6,9 +6,9 @@ import { useNewRef } from '../utils/useNewRef';
 type ResizeOptions<T extends HTMLElement> = Parameters<typeof useResizeObserver<T>>[0];
 
 /**
- * @see {@link https://malkiii.github.io/react-pre-hooks/docs/hooks/useSize | useSize} hook.
+ * @see {@link https://malkiii.github.io/react-pre-hooks/docs/hooks/useRect | useRect} hook.
  */
-export const useSize = <T extends HTMLElement = HTMLDivElement>(args: ResizeOptions<T>) => {
+export const useRect = <T extends HTMLElement = HTMLDivElement>(args: ResizeOptions<T>) => {
   const targetRef = useNewRef<T>(args.ref);
   const [rect, setRect] = useState<DOMRect>();
 
