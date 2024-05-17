ec837b286e0f0dcbd7954799a462babf0f681fe9
malkiAbdoo <abdo.malkiep@gmail.com>
2024-05-17 11:59:43 +0100

    docs: disable next image optimization

--- docs/next.config.js
+++ docs/next.config.js
@@ -3,15 +3,15 @@ const { name, homepage } = require('../packages/hooks/package.json');
 /** @type {import('nextra').NextraConfig} */
 const nextraConfig = {
   theme: 'nextra-theme-docs',
-  themeConfig: './src/theme.config.tsx',
-  staticImage: true
+  themeConfig: './src/theme.config.tsx'
 };
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   transpilePackages: [name],
-  basePath: new URL(homepage).pathname
+  basePath: new URL(homepage).pathname,
+  images: { unoptimized: true }
 };
 
 module.exports = require('nextra')(nextraConfig)(nextConfig);
